c7a77205c02a200ed6e29b2b2147a28430e4d400
Aurélien Grosdidier <aurelien.grosdidier@gmail.com>
2022-05-25 07:43:37 +0200

    feature: new params for graphviz + solves Technologicat#70
    cf. https://github.com/maciejczyzewski/pyan/commit/ac1bd55d07fd1bad2f4a92dc0809607c407d9140

--- visualize_pyan_architecture.sh
+++ visualize_pyan_architecture.sh
@@ -1,4 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
 echo -ne "Pyan architecture: generating architecture.{dot,svg}\n"
-python3 -m pyan pyan/*.py --no-defines --uses --colored --annotate --dot -V >architecture.dot 2>architecture.log
+python3 \
+  -m pyan pyan/*.py \
+  --no-defines --uses --colored --annotate --dot -V \
+  >architecture.dot \
+  2>architecture.log
 dot -Tsvg architecture.dot >architecture.svg
+
+python3 -m pyan pyan/*.py --no-defines --uses \
+  --grouped --nested-groups \
+  --graphviz-layout fdp \
+  --colored --html >architecture.html
